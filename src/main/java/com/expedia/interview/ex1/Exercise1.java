9e2f7d0cecc5ac73ff09ffe30da668c0af70233e
liubaohuagood <liubaohuagood@163.com>
2019-03-15 19:51:47 +0800

    Deng Hongping code-at-home

--- src/main/java/com/expedia/interview/ex1/Exercise1.java
+++ src/main/java/com/expedia/interview/ex1/Exercise1.java
@@ -1,16 +1,45 @@
 package com.expedia.interview.ex1;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 public class Exercise1 {
 
+    /**
+     * At minutes == 0 use "o'clock"
+     * For 1 <= minutes <= 30 use "past"
+     * For 30 < minutes use "to".
+     * The word "minute(s)" can be singular or plural.
+     *
+     * @param h
+     * @param m
+     * @return
+     */
     String timeInWords(int h, int m) {
-        return "";
+
+        String[] words = {
+                "", "one", "two", "three", "four", "five", "six", "seven",
+                "eight", "nine", "ten", "eleven", "twelve", "thirteen",
+                "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+                "nineteen", "twenty", "twenty one", "twenty two", "twenty three",
+                "twenty four", "twenty five", "twenty six", "twenty seven",
+                "twenty eight", "twenty nine"
+        }
+
+        String result = "";
+
+        if (m == 0) {
+            result = words[h] + " o'clock";
+        } else if (m == 15) {
+            result = "quarter past " + words[h];
+        } else if (m == 30) {
+            result = "half past " + words[h];
+        } else if (m == 45) {
+            result = "quarter to " + words[h + 1];
+        } else if (m == 1) {
+            result = "one minute past " + words[h];
+        } else if (m < 30) {
+            result = words[m] + " minutes past " + words[h];
+        } else {
+            result = words[60 - m] + " minutes to " + words[h + 1];
+        }
+        return result;
     }
 }
--- src/main/java/com/expedia/interview/ex2/Exercise2.java
+++ src/main/java/com/expedia/interview/ex2/Exercise2.java
@@ -1,20 +1,33 @@
 package com.expedia.interview.ex2;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 public class Exercise2 {
 
     class SinglyLinkedListNode {
         SinglyLinkedListNode next;
     }
 
+
+    /**
+     * Note: If the list is empty, head will be null.
+     * If the list contains a cycle, your function must return true.
+     * If the list does not contain a cycle, it must return false.
+     *
+     * @param head
+     * @return
+     */
     boolean hasCycle(SinglyLinkedListNode head) {
+        if (null == head) {
+            return false;
+        }
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head;
+        while (null != fast.next && null != fast.next.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
         return false;
     }
 }
